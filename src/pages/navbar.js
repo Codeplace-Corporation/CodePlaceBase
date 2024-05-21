34eb29e4a634fc434f9d898c97de64746aaf7a0d
Dsp672CodePlace <diegorafaelpitt@gmail.com>
2024-05-21 11:14:32 -0700

    NavBar Changes

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,43 +1,44 @@
-
-
 import React from 'react';
-import Auth from '../Auth';
-export default function Navbar({isAuthenticated}) {
+import './Navbar.css';
 
-return <nav className="nav">
-{isAuthenticated ? (<a href="/LogLand" className="site-title"><img border="0" alt="CodePlace" src="CodeLogo.png" width="50" /></a>):(<a href="/" className="site-title"><img border="0" alt="CodePlace" src="CodeLogo.png" width="50" /></a>)}
-
-{isAuthenticated ? (
-  <ul>
- 
-  <li>
-<a href="/SearchJobs">Search Jobs</a>
-</li>
-<li>
-<a href="/MyJobs">My Jobs</a>
-</li>
-
-
-<li>
-<a href="/Messages">Messages</a>
-</li>
-<li>
-<a href="/Profile">Profile</a>
-</li>
-</ul>
-      ):(
-      
+export default function Navbar({ isAuthenticated }) {
+  return (
+    <nav className="nav">
+      <div className="nav-left">
+        <a href={isAuthenticated ? "/LogLand" : "/"}>
+          <img src="CodeLogo.png" alt="CodePlace" />
+        </a>
+      </div>
+      <div className="nav-center">
         <ul>
-        <li>
-        <a href="/searchJobs">Search Jobs</a>
-      </li>
-      <li>
-      <a href="/about">About</a>
-        </li>
-        <li>
-      <a href="/signIn">Sign In</a>
-        </li>
+          <li>
+            <a href="/searchJobs">Search Jobs</a>
+          </li>
+          {isAuthenticated && (
+            <>
+              <li>
+                <a href="/MyJobs">My Jobs</a>
+              </li>
+              <li>
+                <a href="/Messages">Messages</a>
+              </li>
+              <li>
+                <a href="/Profile">Profile</a>
+              </li>
+            </>
+          )}
         </ul>
-      )}
-</nav>
+      </div>
+      <div className="nav-right">
+        {!isAuthenticated ? (
+          <>
+            <a href="/about">About</a>
+            <a href="/signIn">Sign In</a>
+          </>
+        ) : (
+          <a href="/logout">Logout</a>
+        )}
+      </div>
+    </nav>
+  );
 }
