53c7c33bed1f8fd89d0b3282f65ea23c2aff400a
cryvax <aaladan453@gmail.com>
2024-09-01 07:52:42 +0100

    setup layout

--- src/pages/messages/Messages.tsx
+++ src/pages/messages/Messages.tsx
@@ -4,35 +4,56 @@ import { StyledInput } from "../../components/styled/StyledInput";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Messages = () => {
+    // States go here 🦆
+
+    const Conversations = () => {
+        return <div>{/* Return list of conversations */}</div>;
+    };
+    const Messages = () => {
+        return (
+            <div>{/* Return list of messages for selected conversation */}</div>
+        );
+    };
+
     return (
         <div className="grid grid-cols-profile h-screen pt-20 pb-10">
-            <div className="bg-card-dark rounded-md pt-3 px-3">
-                <div className="flex mb-4">
-                    <StyledInput
-                        type="text"
-                        inputSize="small"
-                        variant="filled"
-                        placeholder="Search conversations"
-                        className="w-full"
-                    />
-                    <StyledButton
-                        size="small"
-                        variant="primary"
-                        className="rounded-full bg-primary"
-                    >
-                        <FontAwesomeIcon
-                            icon={faSearch}
-                            className="text-white"
+            <div className="flex flex-col bg-card-dark rounded-md pt-3 px-3">
+                <form
+                    action="#"
+                    onSubmit={() => console.log("Search submitted")}
+                >
+                    <div className="flex mb-3">
+                        <StyledInput
+                            type="text"
+                            inputSize="small"
+                            variant="filled"
+                            placeholder="Search conversations"
+                            className="w-full"
                         />
-                    </StyledButton>
+                        <StyledButton
+                            size="small"
+                            variant="primary"
+                            className="rounded-full bg-primary"
+                            type="submit"
+                        >
+                            <FontAwesomeIcon
+                                icon={faSearch}
+                                className="text-white"
+                            />
+                        </StyledButton>
+                    </div>
+                </form>
+                <div className="flex-1 w-full">
+                    <Conversations />
                 </div>
-                <div> {/* Show list of conversations */} </div>
             </div>
             <div className="flex flex-col bg-card rounded-md">
                 <div className="bg-card-dark py-4 px-3 flex flex-col items-end">
                     <span>Messenger</span>
                 </div>
-                <div>{/* Show the chat here */}</div>
+                <div className="flex-1 w-full">
+                    <Messages />
+                </div>
             </div>
         </div>
     );
