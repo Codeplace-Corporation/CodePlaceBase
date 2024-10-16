e24f076468390286e0350a3362fff69e7f3b4224
cryvax <aaladan453@gmail.com>
2024-10-16 22:24:45 +0100

    complete ui profile card

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,19 +1,8 @@
-import { useEffect, useState } from "react";
-import { auth } from "../../utils/firebase";
 import ProfileCard from "./components/ProfileCard";
+import { useAuth } from "../../context/AuthContext";
 
 const Profile = () => {
-    const [user, setUser] = useState(auth.currentUser);
-
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            setUser(user);
-        });
-
-        return () => {
-            unsubscribe();
-        };
-    }, [user]);
+    const { currentUser } = useAuth();
 
     return (
         <div className="grid grid-cols-profile gap-5 h-screen pb-20 pt-4">
